268fff3a8a677f6c1614137389461629b449dea2
johnram528 <johnram528@gmail.com>
2015-10-31 21:25:43 +0000

    nitrous-backup at Sat Oct 31 21:25:43 UTC 2015

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,9 @@
 def using_until
     levitation_force = 6
-  unti levitation_force == 10
+  until levitation_force == 10
   puts "Wingardium Leviosa" #your code here
-  levitation_force +=1
+  levitation_force += 1
 end
+end
+
 
