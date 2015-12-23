562bc16d913157f1abe1da111b282d711342cb7a
sigkapphi <sigkapphi@gmail.com>
2015-12-23 19:41:28 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
 def using_until
-        levitation_force = 6
-        #your code here
-    
+   levitation_force = 6
+  until levitation_force == 10 do
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
 end
-
+#this one took me alot longer than it should have, because i didnt realize i mispelled Winguardium. This is the second time ive made this kind of mistake. it took me like 2 minutes to write the actual code for both, but probably an hour and some help from someone who didnt catch it either, to finally get this to pass -_-
+#i almost forgot, but loops and times commands need do to go after it.
